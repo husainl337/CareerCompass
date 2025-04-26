14b38e63d5eefffc123c11cf3ef1f791b3481714
Husain <habibi128@gmail.com>
2025-04-26 23:18:29 +0530

    added components to src

---
+++ frontend/src/features/feedback/feedbackSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// Initial state
+const initialState = {
+    sentiment: null,
+}
+
+export const feedbackSlice = createSlice({
+    name: 'feedback',
+    initialState,
+    reducers: {
+        feedbackState:(state,action) => {
+            state.sentiment = action.payload;
+        }
+    }
+})
+
+export const {feedbackState} = feedbackSlice.actions;
+
+export default feedbackSlice.reducer; 
